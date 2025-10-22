15d0d27045082a4f016ce289564fbca905271ec2
TW - Jackson Ruiz <jackson.ruiz@thoughtworks.com>
2025-10-22 11:34:07 -0500

    Make some advance in top navbar

--- src/molecules/mainNavbar/MainNavbar.stories.tsx
+++ src/molecules/mainNavbar/MainNavbar.stories.tsx
@@ -5,9 +5,22 @@ const meta: Meta<typeof MainNavbar> = {
   title: 'Molecules/MainNavbar',
   component: MainNavbar,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
+  parameters: {
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile (375px)',
+          styles: { width: '375px', height: '667px' },
+        },
+        tablet: {
+          name: 'Tablet (768px)',
+          styles: { width: '768px', height: '1024px' },
+        },
+        desktop: {
+          name: 'Desktop (1280px)',
+          styles: { width: '1280px', height: '800px' },
+        },
+      },
     },
   },
 };
@@ -17,7 +30,13 @@ type Story = StoryObj<typeof MainNavbar>;
 
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'MainNavbar',
+    logo: {
+      text: "LONG CONTENT LOGO",
+      shortText: "SL"
+    },
+    avatar: {
+      alt: "Jackson Ruiz",
+      src: "https://media.licdn.com/dms/image/v2/C5603AQEh3ne8L2GoJQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516812791033?e=1762992000&v=beta&t=-YVPU3POC037N1EBm9pS_6FSLNE3I7bQ5BiSjOO6B_s"
+    }
   },
 };
--- src/molecules/mainNavbar/MainNavbar.tsx
+++ src/molecules/mainNavbar/MainNavbar.tsx
@@ -1,35 +1,115 @@
-import { Grid } from "@mui/material";
+import { Avatar, Grid } from "@mui/material";
+import Palette from "../../theme/colors/palette";
+import { useState } from "react";
+import IconButton from "../../atoms/iconButton/IconButton";
+import { Menu } from '@mui/icons-material';
 
-const MainNavbar = () => {
+export interface MainNavbarProps {
+  logo: {
+    text: React.ReactNode,
+    shortText: React.ReactNode,
+  },
+  avatar: {
+    alt: string,
+    src: string,
+  },
+}
+
+const MainNavbar = ({ logo, avatar }: MainNavbarProps) => {
+  const [isLeftPanelCollapsed, setIsLeftPanelCollapsed] = useState(false);
+  const leftPanelWidth: { collapsed: number, expanded: number } = {
+    collapsed: 50,
+    expanded: 200
+  };
+  const navbarHeight = 50;
+
   return (
-    <Grid
-      container
-      direction="row"
+    <Grid container
+      direction="column"
       sx={{
-        justifyContent: "space-around",
-        alignItems: "center",
-        backgroundColor: "skyblue"
+        backgroundColor: Palette.primary.active,
+        color: Palette.primary.text?.active,
+        margin: 0,
+        padding: 0,
       }}
-      spacing={1}
     >
       <Grid
         container
         direction="row"
         sx={{
-          justifyContent: "flex-start",
+          justifyContent: "center",
           alignItems: "center",
+          display: { xs: "flex", sm: "none" },
+          height: navbarHeight,
         }}
       >
-        Left content.
+        {logo.text}
       </Grid>
       <Grid
         container
         direction="row"
         sx={{
-          justifyContent: "flex-end",
+          justifyContent: "space-between",
           alignItems: "center",
+          height: navbarHeight,
         }}
-      >Right content</Grid>
+      >
+        <Grid
+          container
+          direction="row"
+          sx={{
+            justifyContent: "flex-start",
+            alignItems: "center",
+            paddingLeft: 2,
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            sx={{
+              paddingRight: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              display: { xs: "none", sm: "flex" },
+              position: "relative",
+              width: isLeftPanelCollapsed ? leftPanelWidth.collapsed : leftPanelWidth.expanded,
+              transition: "width 0.3s ease-in-out",
+            }}
+          >
+            <Grid sx={{
+              position: 'absolute',
+              opacity: isLeftPanelCollapsed ? 0 : 1,
+              transition: 'opacity 0.1s ease-out',
+              whiteSpace: 'nowrap',
+            }}>
+              {logo.text}
+            </Grid>
+            <Grid sx={{
+              position: 'absolute',
+              opacity: isLeftPanelCollapsed ? 1 : 0,
+              transition: 'opacity 0.1s ease-out',
+              whiteSpace: 'nowrap',
+            }}>
+              {logo.shortText}
+            </Grid>
+          </Grid>
+          <IconButton
+            rounded="circle"
+            onClick={() => setIsLeftPanelCollapsed(!isLeftPanelCollapsed)}
+          ><Menu /></IconButton>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          sx={{
+            justifyContent: "flex-end",
+            alignItems: "center",
+            paddingRight: 1,
+          }}
+        >
+          <Avatar alt={avatar.alt} src={avatar.src} />
+        </Grid>
+      </Grid>
     </Grid>
   )
 }
